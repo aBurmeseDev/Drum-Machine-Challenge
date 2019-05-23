63f0cc9dc9b3c9823fe7f2719cdf8ca460358cdf
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-05-22 21:00:07 -0700

    add buttons

--- src/components/app.js
+++ src/components/app.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import Sounds from '../assets/sounds';
+import React, { Component } from "react";
+import Sounds from "../assets/sounds";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      sounds: Object.keys(Sounds),
+      sounds: Object.keys(Sounds)
     };
     this.playSound = this.playSound.bind(this);
   }
 
   playSound(eventObj) {
     // code to be run when click event is fired goes below this line!
+    console.log("button working");
   }
 
   render() {
-    return (
-      <div className="button-container">
-        {/* Components that need to be returned from App go below here ! */}
-        
-      </div>
-    );
+    const buttons = [];
+    for (let i = 0; i < this.state.sounds.length; i++) {
+      buttons.push(
+        <Button sound={this.state.sounds[i]} playSound={this.playSound} />
+      );
+    }
+    return <div className="button-container">{buttons}</div>;
   }
 }
 
